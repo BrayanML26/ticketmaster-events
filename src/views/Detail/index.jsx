205253eb11549501934cdc2e6fcc13d88267e706
BrayanML26 <brayan.26112001@gmail.com>
2024-09-12 22:57:26 +0200

    Feat: First Commit about the detail pages.

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styles from './Detail.module.css';
+import { format } from 'date-fns';
+import { es } from "date-fns/locale";
 
 const Detail = () => {
     const { eventId } = useParams();
@@ -27,8 +29,29 @@ const Detail = () => {
         fetchEventData();
     }, []);
 
+    if (isLoading && Object.keys(eventData) === 0) {
+        return <div>Cargando evento...</div>
+    }
+
+    if (Object.keys(error) > 0) {
+        return <div>Ha ocurrido un error...</div>
+    }
+
     console.log(eventData);
-    return <div>Detail</div>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.mainInfoContainer}>
+                <img src={eventData.images?.[0].url} />
+                <h4>{eventData.name}</h4>
+                <p>{eventData.info}</p>
+                <p> {eventData?.dates?.start?.dateTime && !isNaN(new Date(eventData.dates.start.dateTime).getTime()) 
+                    ? format(new Date(eventData.dates.start.dateTime), 'd LLLL yyyy H:mm', { locale: es })
+                    : 'Fecha no disponible'}hrs
+                </p>
+
+            </div>
+        </div>
+    );
 }
 
 export default Detail;
